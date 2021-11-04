5746edeca0b80c87fe748ad0bb15591e9456fd50
yangjongtae <jtyang0227@gmail.com>
2021-11-04 20:08:51 +0900

    Board CRUD Add

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board } from './boards.model';
+import { Board, BoardStatus } from './boards.model';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
@@ -17,7 +28,7 @@ export class BoardsController {
   }
 
   /**
-   * 전체 목록 호출
+   * 전체 게시물 가져오기
    */
   @Get('/')
   getAllBoards(): Board[] {
@@ -28,8 +39,49 @@ export class BoardsController {
    * 게시물 생성
    */
   @Post()
-  createBoard(@Body() createBoardDto: CreateBoardDto) {
+  @UsePipes(ValidationPipe)
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardsService.createBoard(createBoardDto);
   }
+
+  /**
+   * 단일 게시물 가져오기
+   * @param id
+   */
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
+  }
+
+  /**
+   * 게시물 삭제
+   * @param id
+   */
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
+  }
+
+  /**
+   *
+   * @param id
+   * @param status
+   */
+  @Patch('/:id/status')
+  updateBoardStatus(@Param('id') id: string,
+                    @Body('status') status: BoardStatus,
+  ): Board {
+    return this.boardsService.updateBoardStatus(id, status);
+  }
+
+  /**
+   * 파이브(ParseIntPipe)
+   * @param id
+   */
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return;
+  }
+
 
 }
