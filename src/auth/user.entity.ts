f716f2a55ad3092797c546f46577338008334e4a
yangjongtae <jtyang0227@gmail.com>
2021-11-06 16:02:10 +0900

    password encryption, signIn

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,6 +1,7 @@
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
+// @Unique(['username']) // 유니크값 처리
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,13 +1,30 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-  async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
+  async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
-    const user = this.create({ username, password });
 
-    await this.save(user);
+    // 비밀번호 암호화
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ username, password: hashedPassword });
+
+    // 회원가입 정합성 예외처리
+    try {
+      await this.save(user);
+    } catch (error) {
+      console.log(error);
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+
   }
 }
